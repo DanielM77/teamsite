b0428f55033e86f7b27ae4c235ceee39f86d3439
Daniel Muenstermann <daniel.muenstermann@online.de>
2012-03-29 14:40:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamsite::Application.config.secret_token = '613781f70a1ce771b90b4887180ce95b7536cb6af6263d7fec1bef923192d9599461f57cefad9c05c4bb7fefe271d89023133c72f5ea4d73c275ce4846afe364'
